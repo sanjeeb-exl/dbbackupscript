07bab8f748478df00fc52d0bbafa5d8ce8f891b7
devops81 <devops81@gmail.com>
2022-04-24 22:01:47 +0530

    Added script for backup

---
+++ BackupDB.ps1
@@ -0,0 +1,72 @@
+#
+# ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── I ──────────
+#   :::::: B A C K U P I N G   W I T H   P O W E R S H E L L   A N D   M Y S Q L D U M P : :  :   :    :     :        :          :
+# ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#
+
+#
+# ─── VARIABLES ──────────────────────────────────────────────────────────────────
+#
+
+$dbUser     ="root"
+$dbPassword ="exl@12345"
+$dbHost     ="localhost"
+$dbName     ="northwinddb"
+
+
+#
+# ─── DATE WITH FORMAT TO ADD A OUT SQL FILE ─────────────────────────────────────
+#
+$ACTUAL_DATE               = Get-Date -Format "dd-MM-yyyy hh.mm.ss"
+
+
+#
+# ─── GETTING THE NAME OF THE OUT SQL FILE TO BACKUP ─────────────────────────────
+#
+$PATH_DBNAME_BAKUP          ="C:\DBBackup"
+$FILE_NAME_BACKUP_DBNAME    ="DBNAMEdump_"
+
+$FILE_BACKUP_DBNAME         = $PATH_DBNAME_BAKUP+$FILE_NAME_BACKUP_DBNAME+$ACTUAL_DATE+".sql"
+
+
+#
+# ─── INVOKING THE COMPLETE COMMAND ──────────────────────────────────────────────
+#
+
+#
+# ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── I ──────────
+#   :::::: B A C K U P I N G   W I T H   P O W E R S H E L L   A N D   M Y S Q L D U M P : :  :   :    :     :        :          :
+# ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+#
+
+#
+# ─── VARIABLES ──────────────────────────────────────────────────────────────────
+#
+
+$dbUser     ="root"
+$dbPassword ="exl@12345"
+$dbHost     ="localhost"
+$dbName     ="northwinddb"
+
+
+#
+# ─── DATE WITH FORMAT TO ADD A OUT SQL FILE ─────────────────────────────────────
+#
+$ACTUAL_DATE               = Get-Date -Format "dd-MM-yyyy hh.mm.ss"
+
+
+#
+# ─── GETTING THE NAME OF THE OUT SQL FILE TO BACKUP ─────────────────────────────
+#
+$PATH_DBNAME_BAKUP          ="C:\DBBackup\"
+$FILE_NAME_BACKUP_DBNAME    ="DBNAMEdump_"
+
+$FILE_BACKUP_DBNAME         = $PATH_DBNAME_BAKUP+$FILE_NAME_BACKUP_DBNAME+$ACTUAL_DATE+".sql"
+
+echo $FILE_BACKUP_DBNAME
+
+#
+# ─── INVOKING THE COMPLETE COMMAND ──────────────────────────────────────────────
+#
+
+.\mysqldump.exe --defaults-extra-file=mysql-credentials.cnf -h $dbHost   $dbName > $FILE_BACKUP_DBNAME
